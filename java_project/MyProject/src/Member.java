6364527d0e8f8fcc7309096126601fa4c6cec85d
Gamaspin <rhrn911@gmail.com>
2021-05-24 16:10:53 +0900

    Update Member.java

--- java_project/MyProject/src/Member.java
+++ java_project/MyProject/src/Member.java
@@ -1,6 +1,15 @@
 public class Member {
 
 	class Calculator {
+		void plus(int num1, int num2) {
+			System.out.println(num1 + num2);	}
+		void minus(int num1, int num2) {
+			System.out.println(num1 - num2);	}
+		void multi(int num1, int num2) {
+			System.out.println(num1 * num2);	}
+		void div(int num1, int num2) {
+			System.out.println(num1 / num2);	}
+						
 		int add (int a, int b) { 
 			return a + b;	}
 		int subtract (int a, int b) {
