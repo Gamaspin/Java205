fae504ae8ccbfd028422c7aeae49cd3d9ecf3f25
Gamaspin <rhrn911@gmail.com>
2021-05-24 16:58:17 +0900

    1챕터
    연습문제

--- java_project/MyProject/src/Calculator.java
+++ java_project/MyProject/src/Calculator.java
@@ -2,32 +2,34 @@ public class Calculator {
 
 	//메소드 -> 기능 (재료 -> 처리 -> 반환)
 	//반환타입 메소드이름(매개변수들) {	처리 구문	}
-		void plus(int num1, int num2) {
-			System.out.println(num1 + num2);	}
-		void minus(int num1, int num2) {
-			System.out.println(num1 - num2);	}
-		void multi(int num1, int num2) {
-			System.out.println(num1 * num2);	}
-		void div(int num1, int num2) {
-			System.out.println(num1 / num2);	}
-						
-		int add (int a, int b) { 
-			return a + b;	}
-		int subtract (int a, int b) {
-			return a - b;	}
-		int multiply (int a, int b) {
-			return a * b;	}
-		int divide (int a, int b) { 
-			return a / b;	}
-	
+	void plus(int num1, int num2) {
+		System.out.println(num1 + num2);	}
+	void minus(int num1, int num2) {
+		System.out.println(num1 - num2);	}
+	void multi(int num1, int num2) {
+		System.out.println(num1 * num2);	}
+	void div(int num1, int num2) {
+		System.out.println(num1 / num2);	}
+
+	int add (int a, int b) { 
+		return a + b;	}
+	int subtract (int a, int b) {
+		return a - b;	}
+	int multiply (int a, int b) {
+		return a * b;	}
+	int divide (int a, int b) { 
+		return a / b;	}
+
 
 	public static void main(String[] args) {
 		System.out.println("류한성");
-		
+
 		//인스턴스 생성 -> 클래스의 정의가 필요 ( 필요한 변수, 필요한 메소드)
 		//클래스이름 참조변수 = new 클래스이름
-		
 		Calculator cal = new Calculator();
-		
+		cal.plus(10, 20);
+		cal.minus(20, 10);
+		cal.multi(5, 6);
+		cal.div(10, 3);
 	}
 }
