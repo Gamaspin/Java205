e00cfd4f40c24f267570d6e8bf47a6417d54bd0c
Gamaspin <rhrn911@gmail.com>
2021-05-24 16:02:51 +0900

    Calculator 클래스
    사칙연산 메소드 추가

---
+++ java_project/MyProject/src/Member.java
@@ -0,0 +1,17 @@
+public class Member {
+
+	class Calculator {
+		int add (int a, int b) { 
+			return a + b;	}
+		int subtract (int a, int b) {
+			return a - b;	}
+		int multiply (int a, int b) {
+			return a * b;	}
+		int divide (int a, int b) { 
+			return a / b;	}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("류한성");
+	}
+}
