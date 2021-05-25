ef2b08917a778260dc5781354d3671561b45a6af
Gamaspin <rhrn911@gmail.com>
2021-05-25 19:02:45 +0900

    Update Member.java

--- java_project/MyProject/src/ch03/Member.java
+++ java_project/MyProject/src/ch03/Member.java
@@ -10,18 +10,19 @@ public class Member {
 	String birthday;
 	String address;
 
-	Member(String name, String phonNo,String major,int grade, String email) {
-		this(name, phonNo, major, grade, email, "7월7일", "Tottenham, England");
+	Member(String name, String phonNo, String major, int grade, String email, String birthday, String address) {
+		this(name, phonNo, major, grade, email);
+		this.birthday = birthday; 
+		this.address = address;
 	}
 	
-	Member(String name, String phonNo, String major, int grade, String email, String birthday, String address) {
+	Member(String name, String phonNo, String major, int grade, String email) {
 		this.name = name;
 		this.phoneNo = phonNo;
 		this.major = major;
 		this.grade = grade;
 		this.email = email;
-		this.birthday = birthday;
-		this.address = address;
+		
 	}
 
 	void printInfo() {
@@ -35,8 +36,8 @@ public class Member {
 	}
 
 	public static void main(String[] args) {
-		Member m1 = new Member("손흥민", "010-3333-8888", "양발감아차기", 4, "hmson7@");
-		Member m2 = new Member("이강인", "010-1111-7777", "탈압박메커니즘", 1, "kanginlee@", "5월 25일", "Valencia, Spain");
+		Member m1 = new Member("손흥민", "010-3333-8888", "양발감아차기", 4, "hmson7@", "7월 7일", "Tottenham, England");
+		Member m2 = new Member("이강인", "010-1111-7777", "탈압박메커니즘", 1, "kanginlee@");
 		m1.printInfo();
 		System.out.println();
 		m2.printInfo();
