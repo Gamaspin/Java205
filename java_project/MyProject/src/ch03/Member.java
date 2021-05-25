0123ee3626fb559ec8423a9a61c523994cfc2262
Gamaspin <rhrn911@gmail.com>
2021-05-25 18:49:49 +0900

    info
    info add

---
+++ java_project/MyProject/src/ch03/Member.java
@@ -0,0 +1,45 @@
+package ch03;
+
+public class Member {
+	//변수의 기능 1. 데이터를 메모리에 저장 2. 주소의기능
+	String name;
+	String phoneNum;
+	String major;
+	int grade;
+	String email;
+	String birthday;
+	String address;
+
+	Member(String name, String phonNum,String major,int grade, String email) {
+		this(name, phonNum, major, grade, email, "7월7일", "Tottenham, England");
+	}
+	
+	Member(String name, String phonNum,String major,int grade, String email, String birthday, String address) {
+		this.name= name;
+		this.phoneNum = phonNum;
+		this.major=major;
+		this.grade = grade;
+		this.email = email;
+		this.birthday = birthday;
+		this.address = address;
+	}
+
+	void printInfo() {
+		System.out.println("이름 : "+name);
+		System.out.println("휴대폰번호 : "+phoneNum);
+		System.out.println("전공 : "+major);
+		System.out.println("학년 : "+grade);
+		System.out.println("email : "+email);
+		System.out.println("생일 : "+birthday);
+		System.out.println("주소 : "+address);
+	}
+
+	public static void main(String[] args) {
+		Member m1 = new Member("손흥민", "010-3333-8888", "양발감아차기", 4, "hmson7@");
+		Member m2 = new Member("이강인", "010-1111-7777", "탈압박메커니즘", 1, "kanginlee@", "5월 25일", "Valencia, Spain");
+		m1.printInfo();
+		System.out.println();
+		m2.printInfo();
+	}
+}
+
