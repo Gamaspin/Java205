a8d453ee396e6656193c3bcbf04f9b3bcf290821
Gamaspin <rhrn911@gmail.com>
2021-05-26 18:12:04 +0900

    Update Person_Main.java

--- java_project/MyProject/src/ch06/Person_Main.java
+++ java_project/MyProject/src/ch06/Person_Main.java
@@ -2,7 +2,7 @@ package ch06;
 
 public class Person_Main {
 	public static void main(String[] args) {
-		Male male= new Male("손흥민", 30 , "921234-1122334","아이유");
+		Male male = new Male("손흥민", 30 , "921234-1122334","아이유");
 		Female female = new Female("아이유", 27, "961234-2045678","BTS");
 
 		male.hello();
