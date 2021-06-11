da8734634500bcc7559a9027fc1066a46a10ec74
Gamaspin <rhrn911@gmail.com>
2021-06-11 15:29:59 +0900

    SQL database example

--- database/20210611_function2.sql
+++ database/20210611_function2.sql
@@ -31,11 +31,45 @@ order by publisher
 -- where 조건(연산자)
 -- group by 그룹핑할 기준의 칼럼명;
 
+-- 부서별 급여 평균을 구할 시
+select avg(sal) from emp where deptno=10;
+select avg(sal) from emp where deptno=20;
+select avg(sal) from emp where deptno=30;
+
+select deptno as "부서명", round(avg(sal),2) as "부서의 급여 평균", count(*) as "부서 인원"
+        , min(sal) as "부서의 최소 급여액", max(sal) as "부서의 최대 급여액"
+from emp
+group by deptno
+order by deptno
+;
 
 
 
+select job
+from emp
+group by job
+order by job
+;
 
 
+select deptno, count(*), count(comm)
+from emp
+group by deptno
+order by deptno
+;
 
+select deptno as "부서명", round(avg(sal),2) as "부서의 급여 평균", count(*) as "부서 인원"
+        , min(sal) as "부서의 최소 급여액", max(sal) as "부서의 최대 급여액"
+from emp
+group by deptno
+having avg(sal) >= 2000
+order by deptno
+;
 
+select deptno, max(sal), min(sal)
+from emp
+group by deptno
+having max(sal) >= 2900
+order by deptno
+;
 
