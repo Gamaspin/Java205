9d0b15ecebe9f6f34f52e5ecd88b5f83cc788f4d
Gamaspin <rhrn911@gmail.com>
2021-06-16 16:42:19 +0900

    DML & DDL

---
+++ database/20210615_DDL_phonebook.sql
@@ -0,0 +1,53 @@
+-- 2021.06.15
+-- DDL 생성하기
+
+
+create table phoneInfo_basic ( 
+    idx number(6) constraint pi_basic_idx_PK primary key,
+    fr_name varchar2(20) not null,
+    fr_phonenumber varchar2(20) not null,
+    fr_email varchar2(20),
+    fr_address varchar2(20),
+    fr_regdate date default sysdate
+    -- , constraint pi_basic_idx_PK primary key(idx)
+);
+
+create table phoneInfo_univ (
+    idx number(6),
+    fr_u_major varchar2(20) default 'N' not null,
+    fr_u_year number(1) default '1' not null, -- check (fr_u_year between 1 and 4),
+    fr_ref number(6) not null,
+    constraint pi_univ_idx_PK primary key (idx),
+    constraint chk check (fr_u_year between 1 and 4), -- 테이블 레벨에서 check 제약 설정 
+    constraint pi_univ_ref_FK foreign key (fr_ref) references phoneInfo_basic (idx)
+);
+
+create table phoneInfo_com (  
+    idx number(6),
+    fr_c_company varchar2(20) default 'N' not null,
+    fr_ref number(6) not null,
+    constraint pi_com_idx_PK primary key(idx),
+    constraint pi_com_fr_ref_FK foreign key (fr_ref) references phoneInfo_basic(idx)
+);       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        
+  
---
+++ database/20210616_Ex1_DML.sql
@@ -0,0 +1,103 @@
+--앞에서 생성한 전화번호부 테이블을 기준으로 DML 을 작성해봅시다.
+
+-- Insert : C reate
+-- Select : R ead
+-- Update : U pdate
+-- Delete : D elete
+
+
+
+--1. phoneInfo_basic 테이블의 SELECT, UPDATE, DELETE, INSERT 하는 SQL
+
+desc phoneinfo_basic;
+
+insert into phoneinfo_basic
+values (1, 'KING', '010-0000-0000', 'king@bitcamp.com', 'KOREA', sysdate)
+;
+
+select * from phoneinfo_basic;
+
+insert into phoneinfo_basic (idx, fr_name, fr_phonenumber)
+values (2, 'SCOTT' ,'010-0000-0001')
+;
+----
+update phoneinfo_basic
+set fr_email = 'scott@bitcamp.com', fr_address = 'Incheon'
+where idx = 2   -- fr_name = 'SCOTT'
+;
+
+update phoneinfo_basic
+set fr_address = 'Seoul'
+where idx = 1
+;
+----
+delete from phoneinfo_basic
+where idx = 1
+;
+
+
+----------
+--2. phoneinfo_univ 테이블의 SELECT, UPDATE, DELETE, INSERT 하는 SQL
+
+desc phoneinfo_univ ;
+
+insert into phoneinfo_basic
+values (3, 'SON', '010-0000-0002', 'son@bitcamp.com', 'KOREA', sysdate)
+;
+insert into phoneinfo_univ
+values (1, 'COMPUTER', 4, 3)
+;
+
+select pb.fr_name, pu.fr_u_major, pu.fr_u_year
+from phoneinfo_basic pb, phoneinfo_univ pu
+where pb.idx = pu.fr_ref
+;
+
+delete from phoneinfo_univ
+where idx = 2
+;
+
+
+--3. phoneinfo_com 테이블의 SELECT, UPDATE, DELETE, INSERT 하는 SQL
+
+desc phoneinfo_com ;
+select * from phoneinfo_com ;
+
+insert into phoneinfo_basic
+values (4, 'PARK', '010-0000-0003', 'park@bitcamp.com', 'Gangwon', sysdate)
+;
+insert into phoneinfo_com
+values (1, 'NAVER', 4)
+;
+
+select pb.fr_name, pu.fr_u_major, pu.fr_u_year, pc.fr_c_company
+from phoneinfo_basic pb, phoneinfo_univ pu, phoneinfo_com pc
+where pb.idx = pc.fr_ref
+;
+----
+update phoneinfo_com
+set fr_c_company = 'KAKAO'
+--where idx = 1
+where fr_ref = 4
+;
+
+-- 지울 땐 밑에꺼(자식) 부터
+delete from phoneinfo_com
+where idx = 1
+;
+delete from phoneinfo_basic
+where idx = 4
+;
+
+
+
+
+
+-- 전체 친구 정보
+select *
+from phoneinfo_basic pb, phoneinfo_univ pu, phoneinfo_com pc
+where pb.idx = pu.fr_ref(+) and pb.idx = pc.fr_ref(+)   --outer join
+;
+
+
+
