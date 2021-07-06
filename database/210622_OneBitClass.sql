75fb7a07b9d826186ba228a00484cb99fdc82867
Gamaspin <rhrn911@gmail.com>
2021-07-06 16:53:20 +0900

    팀프로젝트1

---
+++ database/210622_OneBitClass.sql
@@ -0,0 +1,67 @@
+-- 20210622
+-- One bit Class 테이블 생성
+drop table classmember;
+drop table bitclass;
+drop table classorder;
+
+create table classmember (   -- 회원 정보 저장
+    mno number(4) constraint classmember_mno_pk primary key, -- 회원번호
+    mid varchar2(20) not null,      -- ID
+    mpw varchar2(20) not null,      -- 비밀번호
+    mname varchar2(20) not null,    -- 이름
+    mdate date,                     -- 생년월일 
+    mloc varchar2(20),              -- 선호 지역
+    mpoint integer                  -- 보유 포인트
+);
+
+create table bitclass (             -- 강좌 정보 저장     
+    cno number(4) constraint bitclass_cno__pk primary key, -- 강좌 번호
+    mno number(4) constraint bitclass_mno_fk references classmember(mno), -- 강사 번호
+    title varchar2(50) not null,
+    cloc varchar2(20) not null, -- 강좌 지역
+    startdate date not null, -- 강좌 시작일
+    enddate date not null, -- 강좌 종료일
+    fee integer not null, -- 수강료
+    discount integer, -- 할인 가격
+    rate number(2), -- 평점
+    numpeople number(2) not null -- 수강 인원
+);
+
+create table classorder ( -- 수강 신청 
+    orderno number(4) constraint classorder_on_pk primary key, -- 신청번호
+    mno number(4) constraint classorder_mno_fk references classmember(mno), -- 회원 번호
+    cno number(4) constraint classorder_cno_fk references bitclass(cno), -- 강좌 번호
+    orderdate date default sysdate -- 신청일
+);
+
+create sequence classmember_mno_seq
+start with 1
+increment by 1
+;
+
+create sequence bitclass_cno_seq
+start with 1
+increment by 1
+;
+
+create sequence classorder_orderno_seq
+start with 1
+increment by 1
+;
+
+insert into classmember (mno, mid, mpw, mname, mloc)
+    values (classmember_mno_seq.nextval, 'jongwon', '123456', '성종원', '서울');  
+insert into bitclass (cno, title, cloc, fee, startdate, enddate, numpeople)
+    values (bitclass_cno_seq.nextval, '요리교실', '용산', 50000, '21/06/22',
+            '21/07/22', 10); 
+
+
+select * from classmember;
+select * from bitclass;
+select * from classorder;
+
+commit;
+
+select * from bitclass;
+select * from classmember;
+select * from bitclass natural join classmember where mno = 4 order by cno;
---
+++ java_project/OneBitClass/src/oneBitClass/ClassMain.java
@@ -0,0 +1,50 @@
+package oneBitClass;
+
+import java.util.Scanner;
+
+public class ClassMain {
+
+	public static void main(String[] args) {
+
+//		//ClassMain manager = new ClassMain(ClassDao.getInstance());
+//		ClassMain manager = new ClassMain();
+//		Scanner sc2 = new Scanner(System.in);
+//		Login2 login = new Login2();
+//		
+//			
+//		System.out.println("부서관리 프로그램");
+//		System.out.println("-----------------------------------");
+//		System.out.println("1. 부서 리스트");
+//		System.out.println("2. 부서 정보 입력");
+//		System.out.println("3. 부서 정보 수정");
+//		System.out.println("4. 부서 정보 삭제");
+//		System.out.println("5. 프로그램 종료");
+//		System.out.println("-----------------------------------");
+//		System.out.println("원하시는 기능의 번호를 입력해주세요.");
+//		int num = Integer.parseInt(sc2.nextLine());
+//
+//		switch(num) {
+//		case 1 :
+//			//					manager.deptList();
+//			break;
+//		case 2 :
+//			//					if(manager.login() ) {
+//
+//			//					}
+//
+//
+//
+//			break;
+//		case 3 :
+//			//					manager.editDept();
+//			break;
+//		case 4 :
+//			//					manager.delDept();
+//			break;
+//		case 5 :
+//			System.out.println("프로그램을 종료합니다.");
+//			return;
+//		}
+	}
+}
+
